6ee1ef3f9b003bec5728cef6bcb61a4fb3f46a87
Angel <avegasam@gmail.com>
2022-05-23 01:06:39 -0500

    update

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -13,4 +13,24 @@ export class TaskController {
     create(@Body() taskDTO: TaskDTO) {
         return this.taskService.create(taskDTO);
     }
+
+    @Get()
+    findAll() {
+        return this.taskService.findAll()
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.taskService.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() taskDTO: TaskDTO) {
+        return this.taskService.update(id, taskDTO)
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.taskService.delete(id)
+    }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -7,6 +7,7 @@ import { TaskDTO } from './dto/task.dto';
 export class TaskService {
 
     tasks: ITask[] = [];
+
     create(taskDTO: TaskDTO): ITask {
         const task = {
             id: uuidv4(),
@@ -14,5 +15,24 @@ export class TaskService {
         };
         this.tasks.push(task);
         return task;
+    }
+
+    findAll(): ITask[] {
+        return this.tasks;
+    }
+
+    findOne(id: string): ITask {
+        return this.tasks.find((t) => t.id === id)
+    }
+
+    update(id: string, taskDTO: TaskDTO): ITask {
+        const newTask = { id, ...taskDTO };
+        this.tasks = this.tasks.map((t) => (t.id === id ? newTask : t))
+        return newTask
+    }
+
+    delete(id: string): string {
+        this.tasks = this.tasks.filter((t) => t.id !== id)
+        return 'Task deleted'
     }
 }
